68bc98d69e7ce3a40193181f403e41a91f267228
bfigueroa9@ucol.mx <bfigueroa9@ucol.mx>
2022-10-25 19:48:32 -0500

    Agregar archivos de proyecto.

---
+++ Database/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Database
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\atzin\OneDrive\Escritorio\ejercicio\DBPersona.accdb");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LlenarGrid();
+        }
+
+        void LlenarGrid()
+        {
+            conn.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from TablaPersona by Id", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+            conn.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("Insert into TablaPersona(Nombre, Edad)values('"
+                                                + textBox2.Text + "'," + textBox3.Text + ")", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registro exitosamente guardado.");
+
+            //Limpiar texto
+            LimpiarTexto();
+            LlenarGrid();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        void LimpiarTexto()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("delete from TablePersona where Id=" + textBox1.Text + " ", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registro eliminado.");
+            LimpiarTexto();
+            LlenarGrid();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("update TablePersona set Nombre='+" + textBox2.Text + "', Edad=" + textBox3.Text + " where Id=" + textBox1.Text + " ", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registro actualizado.");
+            LimpiarTexto();
+            LlenarGrid();
+        }
+    }
+}
